3dd62c10e6204c37954d48d1b46ffd61ad59eebd
giuliopanda <giuliopanda@gmail.com>
2023-02-15 11:49:05 +0100

    Deploing: version 1.7.0

--- admin-form-pro.php
+++ admin-form-pro.php
@@ -20,16 +20,19 @@ namespace admin_form;
 
 if (!defined('WPINC')) die;
 
-define('ADFO_PRO_VERSION', '1.6.2');
+define('ADFO_PRO_VERSION', '1.7.0');
 //define('ADFO_PRO_VERSION', rand());
 
+if(is_file(__DIR__ . "/../admin-form/admin-form.php")) {
+    define('ADFO_EXIST', 'OK');
+}
 $idrs = dirname(plugin_dir_path(__FILE__));
-
-require_once(__DIR__ . "/includes/dbp-loader-list-form.php");
-require_once(__DIR__ . "/includes/dbp-loader-list-sql-edit.php");
-require_once(__DIR__ . "/includes/dbp-list-loader.php");
-require_once(__DIR__ . "/includes/adfo-loader-import-export.php");
-
+if (defined('ADFO_EXIST')) {
+    require_once(__DIR__ . "/includes/dbp-loader-list-form.php");
+    require_once(__DIR__ . "/includes/dbp-loader-list-sql-edit.php");
+    require_once(__DIR__ . "/includes/dbp-list-loader.php");
+    require_once(__DIR__ . "/includes/adfo-loader-import-export.php");
+}
 if (is_admin()) {
     require_once( __DIR__ . "/includes/adfo-plugin-updater.php" );
     require_once(__DIR__ . "/database_press/database_press.php");
